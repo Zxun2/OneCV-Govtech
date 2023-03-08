a63e6fb96fd4d8a0b90432a7e7d8e364f2bde868
Zong Xun <zxun2000@gmail.com>
2023-03-09 01:33:52 +0800

    feat: add db services for students and teachers

---
+++ services/student.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"Zxun2/OneCV-Govtech/db"
+	"Zxun2/OneCV-Govtech/errors"
+	"Zxun2/OneCV-Govtech/models"
+)
+
+// SuspendStudent suspends a student given the email
+func SuspendStudent(payload models.SuspendStudentPayload) (response models.SuspendStudentResponse) {
+	_, err := getStudentByEmailAndUpdateStatus(payload.Student)
+	if err != nil {
+		return models.SuspendStudentResponse{
+			Response: errors.MakeResponseErr(models.NotFound),
+		}
+	}
+	return models.SuspendStudentResponse{}
+}
+
+func getStudentByEmailAndUpdateStatus(email string) (*models.Student, error) {
+	student := models.Student{}
+	err :=  db.Store.Model(&models.Student{Email: email}).Find(&student).Update("status", models.SUSPENDED).Error
+	return nil, err
+}
+
+func checkStudentExists(email string) bool {
+	var count int64
+	result := db.Store.Model(&models.Student{Email: email}).Count(&count)
+	return result.Error == nil && count > 0
+}
+
+func getStudentsByEmail(emails []string) ([]models.Student, error) {
+	students := []models.Student{}
+	err := db.Store.Model(&models.Student{}).Where("email IN (?)", emails).Find(&students).Error
+	return students, err
+}
---
+++ services/teacher.go
@@ -0,0 +1,109 @@
+package services
+
+import (
+	"Zxun2/OneCV-Govtech/db"
+	"Zxun2/OneCV-Govtech/errors"
+	"Zxun2/OneCV-Govtech/models"
+	"Zxun2/OneCV-Govtech/utils"
+	"log"
+)
+
+// RegisterStudentsToTeacher registers multiple students to a teacher
+func RegisterStudentsToTeacher(payload models.RegistersStudentsPayload) (response models.SuspendStudentResponse) {
+	teacher, err := getTeacherByEmail(payload.Teacher)
+	log.Println(payload.Students)
+	if err != nil {
+		return models.SuspendStudentResponse{
+			Response: errors.MakeResponseErr(models.ServerError),
+		}
+	}
+
+	students, err := getStudentsByEmail(payload.Students)
+	if err != nil {
+		return models.SuspendStudentResponse{
+			Response: errors.MakeResponseErr(models.ServerError),
+		}
+	}
+
+	if len(students) != len(payload.Students) || teacher.Email == "" {
+		return models.SuspendStudentResponse{
+			Response: errors.MakeResponseErr(models.NotFound),
+		}
+	}
+
+	log.Println(students)
+
+	err = db.Store.Model(&teacher).Association("Students").Append(students)
+	if err != nil {
+		return models.SuspendStudentResponse{
+			Response: errors.MakeResponseErr(models.ServerError),
+		}
+	}
+
+	return models.SuspendStudentResponse{}
+}
+
+func getTeacherByEmail(email string) (*models.Teacher, error) {
+	teacher := models.Teacher{}
+	err :=  db.Store.Model(&models.Teacher{}).Where("email = ?", email).Find(&teacher).Error
+	log.Println(teacher)
+	return &teacher, err
+}
+
+// GetCommonStudents retrieves a list of students common to a given list of teachers
+func GetCommonStudents(teacherEmails []string) ([]string, error) {
+	var students []models.Student
+	err := db.Store.Table("students").
+		Select("students.email").
+		Joins("JOIN teacher_students ON students.id = teacher_students.student_id").
+		Joins("JOIN teachers ON teachers.id = teacher_students.teacher_id").
+		Where("teachers.email IN (?) AND students.status = ?", teacherEmails, "active").
+		Group("students.id, students.email").
+		Having("COUNT(DISTINCT teachers.id) = ?", len(teacherEmails)).
+		Find(&students).Error
+
+		if err != nil {
+			return nil, err
+		}
+
+		var studentEmails []string 
+
+		for i := range students {
+			studentEmails = append(studentEmails, students[i].Email)
+		}
+
+    return studentEmails, nil
+}
+
+// ListStudentsReceiveNotifications retrieves a list of students who can receive a given notification.
+func ListStudentsReceiveNotifications(payload models.ListStudentReceivingNotificationPayload) (models.ListStudentReceivingNotificationResponse) {
+	teacherEmail := payload.Teacher
+	notification := payload.Notification
+
+	studentEmails := utils.ParseEmails(notification)
+
+	log.Println(studentEmails)
+
+	var teacher models.Teacher
+	var students []string = studentEmails
+
+	db.Store.Model(models.Teacher{}).Preload("Students").Where("email = ?", teacherEmail).
+			First(&teacher)
+
+	if teacher.Email == "" {
+			// Handle error: no teacher found for the given email
+			return models.ListStudentReceivingNotificationResponse{
+				Response: errors.MakeResponseErr(models.NotFound),
+			}
+	}
+
+	for i := range teacher.Students {
+			if teacher.Students[i].Status == "active" {
+					students = append(students, teacher.Students[i].Email)
+			}
+	}
+
+	return models.ListStudentReceivingNotificationResponse{
+		Recipients: students,
+	}
+}
